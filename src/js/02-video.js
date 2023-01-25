5649f603d5f4ec8ccaf4f4284a4996623a9b8894
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-25 23:21:32 +0200

    up task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('pause', function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      // seconds = the current playback position
+      console.log(seconds);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
