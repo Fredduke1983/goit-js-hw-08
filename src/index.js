f4ca6902688db2b19a60a51953b8c85c6cbcb388
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-25 22:47:20 +0200

    up

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
