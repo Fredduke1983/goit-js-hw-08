9cf8285ff2c8223b7269470638879203e9a58bc8
Fredduke1983 <fredduke1983@gmail.com>
2023-01-27 01:55:44 +0200

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
-console.log('fasgfsdag');
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', current);
+
+const storage = {
+  email,
+  message,
+};
+let KEY = 'feedback-form-state';
+let VALUE = '';
+
+function current(e) {
+  const currentEl = e.target;
+  storage.email = currentEl.name;
+  storage.message = currentEl.value;
+  //   VALUE = JSON.stringify(storage);
+  //   localStorage.setItem(KEY, VALUE);
+}
