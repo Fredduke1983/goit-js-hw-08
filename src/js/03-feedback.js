1aade145f0e45af7234b74d14da4bcf681da5cc8
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-28 20:50:15 +0200

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,16 +3,17 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', current);
 
 const storage = {
-  email,
-  message,
+  email: '',
+  message: '',
 };
 let KEY = 'feedback-form-state';
 let VALUE = '';
 
 function current(e) {
   const currentEl = e.target;
+  console.log(e.target.value);
   storage.email = currentEl.name;
   storage.message = currentEl.value;
-  //   VALUE = JSON.stringify(storage);
-  //   localStorage.setItem(KEY, VALUE);
+  VALUE = JSON.stringify(storage);
+  localStorage.setItem(KEY, VALUE);
 }
