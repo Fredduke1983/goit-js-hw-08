7cedc3fde7601a859d9a7c8cedb74321f9d75d08
Fredduke1983 <fredduke1983@gmail.com>
2023-01-28 18:50:48 +0000

    Deploying to gh-pages from @ Fredduke1983/goit-js-hw-08@1aade145f0e45af7234b74d14da4bcf681da5cc8 🚀

--- 03-feedback.cd17192b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(a){const t=a.target;e.email=t.name,e.message=t.value}));const e={email:email,message:message};
-//# sourceMappingURL=03-feedback.cd17192b.js.map
